675af7157f24f0a673a6c56c33b7e20dc858a14d
Awesome-5-Aayan <81710455+Awesome-5-Aayan@users.noreply.github.com>
2022-08-05 18:29:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,53 @@ function speak(){
     var utterThis= new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
 }
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("emotion1_name").innerHTML=results[0].label;
+        document.getElementById("emotion2_name").innerHTML=results[1].label;
+        prediction1=results[0].label;
+        prediction2=results[1].label;
+        speak();
+        if(results[0].label=="fist"){
+            document.getElementById("emoji1").innerHTML="&#9994;";
+        }
+
+        if(results[0].label=="high 5"){
+            document.getElementById("emoji1").innerHTML="&#9995;";
+        }
+
+        if(results[0].label=="snap"){
+            document.getElementById("emoji1").innerHTML="&#128076;";
+        }
+
+        if(results[0].label=="thumps up"){
+            document.getElementById("emoji1").innerHTML="&#128077;";
+        }
+
+
+        if(results[1].label=="fist"){
+            document.getElementById("emoji2").innerHTML="&#9994;";
+        }
+
+        if(results[1].label=="high 5"){
+            document.getElementById("emoji2").innerHTML="&#9995;";
+        }
+
+        if(results[1].label=="snap"){
+            document.getElementById("emoji2").innerHTML="&#128076;";
+        }
+
+        if(results[1].label=="thumps up"){
+            document.getElementById("emoji2").innerHTML="&#128077;";
+        }
+    }
+}
